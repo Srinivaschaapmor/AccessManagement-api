b2b28f43c8c9d65eadf67e7d756f6faaff995495
Srinivaschaapmor <srinivasch@aapmor.com>
2024-08-20 12:45:36 +0530

    Updated mail password

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'aapmorblogs@gmail.com'
-app.config['MAIL_PASSWORD'] = 'vzyolkoiczhkmixa'
+app.config['MAIL_PASSWORD'] = 'imewdrnoocxkjqvr'
 
 mail=Mail(app)
 jwt=JWTManager()
